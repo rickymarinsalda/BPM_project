96e50157e32adaf9529b246681ab1f1144a9db02
Ricky Marinsalda <rickymarinsalda@gmail.com>
2023-08-25 17:41:46 +0200

    pusho roba

---
+++ website/src/routes/+page.svelte
@@ -0,0 +1,56 @@
+<script>
+	
+	import { Button, Card, Carousel, Gallery, GalleryItem } from "flowbite-svelte"
+
+
+</script>
+
+
+<div class="flex flex-col md:flex-row justify-around items-center mx-auto mb-5">
+	<div class="md:w-2/5 max-w-2xl">
+		<h1 class="mb-4 text-4xl font-extrabold leading-none tracking-tight text-zinc-900 md:text-5xl lg:text-6xl dark:text-white">
+			Summary review analysis
+		</h1>
+		<p class="md:my-10 my-3 text-lg dark:text-zinc-200">
+			bla bla bla
+		</p>
+	</div>
+	
+</div>
+
+
+
+<hr class="h-px my-12 bg-zinc-200 border-0 dark:bg-zinc-700">
+
+<div id="gallery">&nbsp;</div>
+<div class="container lg:px-32  mx-auto items-center">
+
+	<h1 class="my-10 text-4xl font-extrabold leading-none tracking-tight text-zinc-900 md:text-5xl lg:text-6xl dark:text-white">
+		REVIEWS
+	</h1>
+
+	
+</div>
+
+
+<hr class="h-px my-12 bg-zinc-200 border-0 dark:bg-zinc-700">
+
+
+<div id="whereWeAre">&nbsp;</div>
+<div class="mx-auto my-10">
+
+	<h1 class="my-10 text-4xl font-extrabold leading-none tracking-tight text-zinc-900 md:text-5xl lg:text-6xl dark:text-white">
+		SUMMARY
+	</h1>
+
+
+
+	
+</div>
+<hr class="h-px my-0 bg-zinc-200 border-0 dark:bg-zinc-700">
+<div id="whoIam">&nbsp;</div>
+
+
+  
+<hr class="h-px my-12 bg-zinc-200 border-0 dark:bg-zinc-700">
+
